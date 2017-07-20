387d84cc00d61910bdad4259106c92d0fc312ba7
Max Medina <maxmedina05@gmail.com>
2017-07-20 10:25:26 -0400

    update hello message with real user name

--- pizzaapp/public/js/app.js
+++ pizzaapp/public/js/app.js
@@ -3,18 +3,29 @@
 
   document.addEventListener("DOMContentLoaded", function(event) {
 
-    var loginLink = document.getElementById('login-link');
-    var helloMessage = document.getElementById('hello-message');
-    var btnLogout = document.getElementById('btn-logout');
+    var loginLink           = document.getElementById('login-link');
+    var signupLink           = document.getElementById('signup-link');
+    var helloMsgDropdown    = document.getElementById('hello-message-dropdown');
+    var helloMessage        = document.getElementById('hello-message');
+    var btnLogout           = document.getElementById('btn-logout');
+    var reOrderLink         = document.getElementById('re-order-link');
 
     btnLogout.onclick = logout;
 
     if(isUserLogged()) {
       loginLink.className = "hide";
-      helloMessage.classList.remove("hide");
+      signupLink.className = "hide";
+      helloMsgDropdown.classList.remove("hide");
+
+      var name = localStorage.getItem('name').split(' ');
+
+      helloMessage.innerHTML = (name.length > 1) ? 'Hello, ' + name[0] : 'Hello, ' + name;
+
     } else {
       loginLink.classList.remove("hide");
-      helloMessage.className = "hide";
+      signupLink.classList.remove("hide");
+      helloMsgDropdown.className = "hide";
+      reOrderLink.setAttribute('href', '/login');
     }
 
     function logout(evt){
@@ -23,9 +34,9 @@
       localStorage.removeItem('email');
       localStorage.removeItem('userId');
       localStorage.removeItem('name');
-      
+
       loginLink.classList.remove("hide");
-      helloMessage.className = "hide";
+      helloMsgDropdown.className = "hide";
       location.assign('/');
     }
 
