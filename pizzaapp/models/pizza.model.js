adffbaaed7bde51c56ab3b446ca6f9da821311c3
Max Medina <maxmedina05@gmail.com>
2017-07-20 15:40:30 -0400

    Add build description

--- pizzaapp/models/pizza.model.js
+++ pizzaapp/models/pizza.model.js
@@ -3,9 +3,25 @@ function Pizza(size, ingredients) {
   this.ingredients = ingredients;
 
   this.title = buildTitle(size, ingredients);
+  this.description = buildDescription(size, ingredients);
+
+  this.buildTitle = buildTitle;
+  this.buildDescription = buildDescription;
+
+  return this;
 
   function buildTitle(size, ingredients) {
     return  size + ' ' + ingredients.cheese + ', ' + ingredients.toppings + ' pizza with ' + ingredients.crust + ' crust';
+  }
+
+  function buildDescription(size, ingredients) {
+    var content = 'This pizza has: \n\n';
+    content += 'Size: ' +  size + '\n';
+    content += 'Crust: ' +  ingredients.crust + '\n';
+    content += 'Sauce: ' +  ingredients.sauce + '\n';
+    content += 'Cheese: ' +  ingredients.cheese + '\n';
+    content += 'Toppings: ' +  ingredients.toppings + '\n';
+    return content;
   }
 }
 
