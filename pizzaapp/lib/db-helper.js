d582cf32d3c2c29c26a7ad334153a4ca830ebb3b
Max Medina <maxmedina05@gmail.com>
2017-07-20 10:24:23 -0400

    refactor execute logic

--- pizzaapp/lib/db-helper.js
+++ pizzaapp/lib/db-helper.js
@@ -5,8 +5,9 @@ const assert = require('assert');
 const ORDER = "orders";
 const USER = "users";
 const OFFER = "offers";
+const PIZZA = 'pizzas';
 
-function execute(statement, params, callback) {
+function executeOld(statement, params, callback) {
     MongoClient.connect(process.env.DB_CONN_STR, function(err, db) {
         // assert.equal(null, err);
         if (err) {
@@ -14,6 +15,20 @@ function execute(statement, params, callback) {
             callback(err, []);
         } else {
             statement(params, db, callback);
+            db.close();
+            // console.log("connection closed!");
+        }
+    });
+}
+
+function execute(collection, statement, params, callback) {
+    MongoClient.connect(process.env.DB_CONN_STR, function(err, db) {
+        // assert.equal(null, err);
+        if (err) {
+            console.log("Ups, Something happened!");
+            callback(err, []);
+        } else {
+            statement(collection, params, db, callback);
             db.close();
             // console.log("connection closed!");
         }
@@ -35,9 +50,7 @@ function getUser(user, db, callback) {
 }
 
 function addUser(user, db, callback) {
-    db.collection(USER).insertOne(user, function(err, result) {
-        callback(err, result);
-    });
+    db.collection(USER).insertOne(user, callback);
 }
 
 function getUsers(query, db, callback) {
@@ -95,9 +108,7 @@ function updateOrder(order, db, callback) {
 }
 
 function getOrders(query, db, callback) {
-    db.collection(ORDER).find(query).toArray(function(err, data) {
-        callback(err, data);
-    });
+    db.collection(ORDER).find(query).toArray(callback);
 }
 
 function getOffers(query, db, callback) {
@@ -106,53 +117,62 @@ function getOffers(query, db, callback) {
     });
 }
 
+function getCollection(collection, query, db, callback) {
+  db.collection(collection).find(query).toArray(callback);
+}
+
 module.exports = function DBHelper() {
 
     return {
         // Ingredients
         getCrusts: function(callback) {
-            execute(getIngredients, {type:'crust'}, callback);
+            executeOld(getIngredients, {type:'crust'}, callback);
         },
         getToppings: function(callback) {
-            execute(getIngredients, {type: 'topping'}, callback);
+            executeOld(getIngredients, {type: 'topping'}, callback);
         },
         getSauces: function(callback) {
-            execute(getIngredients, {type: 'sauce'}, callback);
+            executeOld(getIngredients, {type: 'sauce'}, callback);
         },
         getCheeses: function(callback) {
-            execute(getIngredients, {type: 'cheese'}, callback);
+            executeOld(getIngredients, {type: 'cheese'}, callback);
         },
         getIngredients: function(callback) {
-          execute(getIngredients, {}, callback);
+          executeOld(getIngredients, {}, callback);
         },
 
         // Order
         addOrder: function(order, callback) {
-            execute(addOrder, order, callback);
+            executeOld(addOrder, order, callback);
         },
         getOrders: function(query, callback) {
-            execute(getOrders, query, callback);
+            executeOld(getOrders, query, callback);
         },
         updateOrder: function(newOrder, callback) {
-          execute(updateOrder, newOrder, callback);
+          executeOld(updateOrder, newOrder, callback);
         },
         // Offer
         getOffers: function(callback) {
-            execute(getOffers, {}, callback);
+            executeOld(getOffers, {}, callback);
         },
         cancelOrder: function(orderId, callback) {
-          execute(cancelOrder, orderId, callback);
+          executeOld(cancelOrder, orderId, callback);
         },
 
         // Users
         getUser: function(user, callback) {
-            execute(getUser, user, callback);
+            executeOld(getUser, user, callback);
         },
         addUser: function(user, callback) {
-            execute(addUser, user, callback);
+            executeOld(addUser, user, callback);
         },
         getUsers: function(query, callback) {
-            execute(getUsers, query, callback);
+            executeOld(getUsers, query, callback);
+        },
+
+        // Pizzas
+        getPizzas: function(query, callback) {
+          execute(PIZZA, getCollection, query, callback);
         }
     };
 };
